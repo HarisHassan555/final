991b07c9c65b38acd16c3097e261b017e866b4bd
HarisHassan55 <harishassan551@gmail.com>
2024-07-31 04:48:56 +0500

    fifth commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
   rootElement
 );
